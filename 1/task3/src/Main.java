77d467aabb77b34e1cc331dddaa2dd5f10f1f042
DimaBud <budilnikov-dima@rambler.ru>
2018-07-15 22:44:03 +0300

    task3

--- 1/task3/src/Main.java
+++ 1/task3/src/Main.java
@@ -7,14 +7,24 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String str,str2;
         str = in.nextLine();
-        for(int i = 0;i < str.length(); i++) {
-        }
-            System.out.println(str);
+        System.out.println(str);
         str2 = in.nextLine();
-        for(int i = 0;i < str2.length(); i++) {
+
+        if (str.indexOf(str2) != -1){
+            System.out.println("Строка " + str + " содержит строку " + str2);
         }
-        System.out.println(str.equalsIgnoreCase(str2));
-
-
+        else {
+            System.out.println("Строка " + str + " не содержит строку " + str2);
+            String temp = "";
+            for ( Character a : str2.toCharArray() ) {
+                if (str.contains(a.toString()) && !temp.contains(a.toString())) {
+                    temp = temp + a.toString();
+                }
+            }
+            System.out.print("Но содержит символы из неё: ");
+            for (char a: temp.toCharArray() ) {
+                System.out.print(a + " ");
+            }
+        }
     }
 }
