5f9fecb7506b44b49280769f60297fa4e696105b
DimaBud <budilnikov-dima@rambler.ru>
2018-07-15 21:37:53 +0300

    task3

--- 1/task3/src/Main.java
+++ 1/task3/src/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-import java.lang.Integer
+import java.lang.Integer;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Integer length = 5;
-        Integer integerLength = 5;
-        Integer[] integerArray = new Integer[integerLength];
-        Integer[] nums = new Integer[length];
-        for (int i = 0; i < 20; i++) {
-            nums[i] = in.nextInt();
-            System.out.println(nums[i]);
-            Integer[] n = new Integer[integerLength];
-            for (int k = 0; k < 20; k++) {
-            n[k]= in.nextInt();
-            }
-            int k = 0;
-            System.out.println(nums[i].equalsIgnoreCase(n[k]));
-            if false 
+        String str,str2;
+        str = in.nextLine();
+        for(int i = 0;i < str.length(); i++) {
+        }
+            System.out.println(str);
+        str2 = in.nextLine();
+        for(int i = 0;i < str2.length(); i++) {
+        }
+        System.out.println(str.equalsIgnoreCase(str2));
 
 
     }
