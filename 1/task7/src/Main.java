49785d1e1f2923c8a325127d895ff7955a1218b7
DimaBud <budilnikov-dima@rambler.ru>
2018-07-17 23:31:34 +0300

    task7

--- 1/task7/src/Main.java
+++ 1/task7/src/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String[] words = str.split("\\s");
+        for(String subStr:words) {
+            int wordCount = 0;
+            Integer i, wordsSize = words.length, strSize = str.length();
+            for (i = 0; i < wordsSize; ++i)
+                if (words[i] != null && !"".equals(words[i]))
+                    ++wordCount;
+            if (wordCount <=1)
+                System.out.println("Невозможно разделить строку на подстроки");
+            System.out.println(subStr);
+        //char[] strToArray = str.toCharArray();
+        //for(int i = 0; i < strToArray.length; i++) {
+            //if (i > 1)
+                //System.out.println(strToArray[i] + " ");
+            //if (i <= 1)
+                //System.out.println("Невозможно разделить строку на подстроки");
+        }
     }
 }
