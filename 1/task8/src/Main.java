cf7ab8aa2315284f23f54ab0101ea3af9ccfddad
DimaBud <budilnikov-dima@rambler.ru>
2018-07-18 23:04:44 +0300

    task8

--- 1/task8/src/Main.java
+++ 1/task8/src/Main.java
@@ -1,25 +1,38 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        File file = new File(task8);
+    public static void main(String[] args);
+    JFileChooser fc = new JFileChooser();{
+        FileReader fr = new FileReader(fc.getSelectedFile());
+        BufferedReader br = new BufferedReader(fr);
+        File file = new File("task8");
         try {
             if(!file.exists()) {
                 file.createNewFile();
                 PrintWriter out = new PrintWriter(file.getAbsoluteFile());
-                
-             finally {
+                Scanner in = new Scanner(System.in);
+                String e;
+                StringBuilder text = new StringBuilder();
+                String str = in.nextLine();
+                while ((str = br.readLine()) != null) {
+                    System.out.println(str + "\n");
+                    text.append(str);
+                    text.append('\n');
+                    JEditorPane textPane;
+                    textPane.setText(text.toString());
+        }catch(IOException e){
+                    throw new RuntimeException(e);
+        }finally{
+                            if (br != null) {
+                                br.close();
+                            }
+                            System.out.println(str);
+                        }
+                    }
 
                 }
-                out.close();
-            }
-        }
-        catch(IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 }
