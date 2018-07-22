f5d7b453503472a472a9351d713343f105fc6a5e
DimaBud <budilnikov-dima@rambler.ru>
2018-07-22 19:45:20 +0300

    task10

---
+++ 1/task10/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileReader fr = new FileReader( "file.txt" );
+        Scanner scan = new Scanner(fr);
+
+    }
+}
