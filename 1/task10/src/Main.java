4286cef199688af1d8be918e0296478a3aef097c
DimaBud <budilnikov-dima@rambler.ru>
2018-07-25 23:27:02 +0300

    task10

--- 1/task10/src/Main.java
+++ 1/task10/src/Main.java
@@ -1,36 +1,46 @@
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        FileReader fr = new FileReader("file.txt");
-        Scanner in = new Scanner(System.in);
-        Scanner scan = new Scanner(fr);
-        int i = 1;
-        while (scan.hasNextLine()) {
-            i++;
-            FileWriter fw = new FileWriter("file2.txt");
-            String str;
-            str = in.nextLine().trim();
-            {
-                if (scan.nextLine().contains(str)) {
-                    String temp = "";
-                    for ( Character a : str.toCharArray() ) {
-                        if (str.contains(a.toString()) && !temp.contains(a.toString())) {
-                            temp = temp + a.toString();
-                        }
-                    }
-                    for (char a: temp.toCharArray() ) {
-                        fw.write(a);
-                    }
+        FileWriter fw = new FileWriter("file.txt");
+        System.out.println("Введите количество строк: ");
+        Scanner console = new Scanner(System.in);
+        Random rand = new Random();
+        Integer amountOfLines = Integer.parseInt(console.nextLine());
+        for (int i = 0; i < amountOfLines; i++){
+            for(int j = 0; j < 10; j++){
+                int lenStr = 3 + rand.nextInt(4);
+                String tempString = "";
+                for (int k = 0; k < lenStr; k++){
+                    Integer tempInt = rand.nextInt(9);
+                    tempString += tempInt.toString();
                 }
+                fw.write(tempString + " ");
             }
-            fr.close();
-            fw.close();
+            fw.write("\n");
         }
+        fw.close();
+        fw = new FileWriter("file2.txt");
+        FileReader fr = new FileReader("file.txt");
+        Scanner scan = new Scanner(fr);
+        System.out.println("Введите строку для поиска: ");
+        String search = console.nextLine().trim();
+        while (scan.hasNextLine()) {
+            String tempString = scan.nextLine().trim();
+            String[] tempStringArray = tempString.split(" ");
+            for (String str: tempStringArray) {
+                tempString = str.contains(search) ? (str + " ") : "";
+                fw.write(tempString);
+            }
+
+        }
+        fr.close();
+        fw.close();
     }
 }
 
